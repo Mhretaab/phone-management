b46857e8de23230f7c66823044119c555fab16e9
Mhretaab <mhretaab_ayenew@epam.com>
2023-05-28 13:08:59 +0200

    project initialized

---
+++ src/test/java/org/mberhe/management/PhoneManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mberhe.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
